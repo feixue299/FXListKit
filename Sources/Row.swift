fe59c836b3d3de4bdff222a7b2eeb6cdf98b3ade
wupengfei <1569485690@qq.com>
2020-02-22 03:40:46 +0800

    add init method

--- Sources/Row.swift
+++ Sources/Row.swift
@@ -10,36 +10,50 @@ import Foundation
 import UIKit
 
 public struct Row {
-    
     public class Property {
         public enum Size {
             case section(value: Int)
             case single(height: CGFloat)
             case custom(size: CGSize)
         }
-        
+
         public var size: Size = .custom(size: CGSize(width: 60, height: 60))
     }
-    
+
     public let cellType: AnyClass
     public let configClosure: ((_ view: UIView) -> Void)?
     public let property = Property()
     public let didSelect: (() -> Void)?
-    
+
+    public init<View: UIView>(cellType: View.Type,
+                              cellConfig: ((_ view: View) -> Void)? = nil,
+                              configPropertyClosure: ((_ property: Property) -> Void)? = nil,
+                              didSelect: (() -> Void)? = nil) {
+        self.cellType = cellType
+        configClosure = { view in
+            if let vi = view as? View {
+                cellConfig?(vi)
+            } else {
+                fatalError("类型不一致")
+            }
+        }
+        configPropertyClosure?(property)
+        self.didSelect = didSelect
+    }
+
     public init<View: UIView, Model>(cellType: View.Type,
                                      modelConfig: (model: Model, configClosure: (_ view: View, _ model: Model) -> Void)? = nil,
                                      configPropertyClosure: ((_ property: Property) -> Void)? = nil,
                                      didSelect: (() -> Void)? = nil) {
         self.cellType = cellType
-        self.configClosure = { (view: UIView) -> Void in
+        configClosure = { (view: UIView) -> Void in
             if let vi = view as? View {
                 modelConfig?.configClosure(vi, modelConfig!.model)
             } else {
                 fatalError("类型不一致")
             }
         }
-        configPropertyClosure?(self.property)
+        configPropertyClosure?(property)
         self.didSelect = didSelect
     }
 }
-
