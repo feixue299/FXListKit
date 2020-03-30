4bcc7162a7e96d9c6c99a3fdb88caaf17c6bf7f0
wupengfei <1569485690@qq.com>
2020-03-30 10:06:01 +0800

    add row key

--- Sources/FXListKitAnimation/ListViewManagerExtension.swift
+++ Sources/FXListKitAnimation/ListViewManagerExtension.swift
@@ -6,19 +6,41 @@
 //
 
 import Foundation
+import UIKit
 #if canImport(FXListKit)
 import FXListKit
 #endif
 
 #if canImport(DifferenceKit)
 import DifferenceKit
+#endif
+
+private let rowKey = "rowKey"
+
+public extension Row {
+    
+    var key: Int? {
+        set { objc_setAssociatedObject(self, rowKey, newValue, .OBJC_ASSOCIATION_ASSIGN) }
+        get { objc_getAssociatedObject(self, rowKey) as? Int }
+    }
+    
+    init<View: UIView, Key: Hashable>(cellType: View.Type,
+                       key: Key,
+                       cellConfig: ((_ view: View) -> Void)? = nil,
+                       configPropertyClosure: ((_ property: Property) -> Void)? = nil,
+                       didSelect: (() -> Void)? = nil) {
+        self.init(cellType: cellType, cellConfig: cellConfig, configPropertyClosure: configPropertyClosure, didSelect: didSelect)
+        self.key = key.hashValue
+    }
+    
+}
 
 public extension ListViewManager {
     
     func reloadWithAnimation() {
-        let data: [ArraySection<Section, Row>] = dataSource.map({ ArraySection(model: $0, elements: $0.rows) })
+        let data: [ArraySection<Section, RowKey>] = dataSource.map({ ArraySection(model: $0, elements: $0.rows.map(RowKey.init)) })
         generateDataSource()
-        let newData: [ArraySection<Section, Row>] = dataSource.map({ ArraySection(model: $0, elements: $0.rows) })
+        let newData: [ArraySection<Section, RowKey>] = dataSource.map({ ArraySection(model: $0, elements: $0.rows.map(RowKey.init)) })
         let changeset = StagedChangeset(source: data, target: newData)
         collectionView?.reload(using: changeset, setData: { [weak self] (data) in
             self?.dataSource = data.map({ $0.model })
@@ -27,6 +49,19 @@ public extension ListViewManager {
 }
 
 extension Section: Differentiable { }
-extension Row: Differentiable { }
-#endif
+
+private struct RowKey: Hashable, Differentiable {
+    let row: Row
+    init(row: Row) {
+        self.row = row
+    }
+    static func == (lhs: RowKey, rhs: RowKey) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(row)
+        hasher.combine(row.key)
+    }
+}
 
